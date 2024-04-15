17a28d26b62ed47f018c8bb2aa3d00c4a29895d9
Or Ouziel <or.ouziel@elastic.co>
2024-04-15 18:33:18 +0300

    update script and add wip ci test

--- deploy/deployment-manager/deploy_service_account.sh
+++ deploy/deployment-manager/deploy_service_account.sh
@@ -1,47 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# set -oe pipefail
+set -e
 
-# Deploying a service account for the CIS GCP integration using Deployment Manager
-
-## Script Overview
-# The script you are about to execute automates the creation of a service account with a builtin roles that holds the necessary permissions for running the CIS GCP integration.
-# The following steps are involved in the script execution:
-
-### Step 1: Enabling Required APIs
-# Before deploying the CIS GCP integration, certain APIs must be enabled. These APIs include:
-# 1. IAM: Required for creating a service account and a custom role to be attached to the compute instance.
-# 2. Deployment Manager: Necessary for creating the deployment itself.
-# 3. Cloud Resource Manager: Enables querying information about the project being analyzed.
-# 4. Cloud Asset Inventory: Facilitates data collection about assets linked to the project.
-
-### Step 2: Applying the Deployment Manager Templates
-# Upon executing the script, the following resources will be created:
-# 1. service account with bindings for roles/cloudasset.viewer" and "roles/browser"
-# 2. service account key
-
-### Step 3: Saving the Service Account Key
-# After the script execution is complete, a KEY_FILE.json is generated and the user is instructed to copy and paste the key in the CIS GCP integration.
-
-# In case the deployment encounters any issues and fails, the script will attempt to delete the deployment along with all the associated resources that were created during the process.
+# this script:
+# 1. enables necessary APIs for CSPM GCP integration
+# 2. applies a Deployment Manager template to create a service account with role and a key
+# 3. saves generated key to a file (KEY_FILE.json) and prompts the user to copy-paste it to the GCP integration
+# 4. handles deployment failure cleanup
 
 DEPLOYMENT_LABELS=${DEPLOYMENT_LABELS:-type=cspm-gcp}
 DEPLOYMENT_NAME=${DEPLOYMENT_NAME:-elastic-agent-cspm-user}
-
-# Set environment variables with the name and number of your project.
 PROJECT_NAME="$(gcloud config get-value core/project)"
 PROJECT_NUMBER="$(gcloud projects list --filter="${PROJECT_NAME}" --format="value(PROJECT_NUMBER)")"
 
 export PROJECT_NAME
 export PROJECT_NUMBER
-
-# Function to run a gcloud command and check its exit code
-run_command() {
-    eval "$1"
-    local status=$?
-    if [ "$status" -ne 0 ]; then
-        echo "Error: Command \"$1\" failed with exit code $status. Exiting..."
-        exit "$status"
-    fi
-}
 
 configure_scope() {
     if [ -n "$ORG_ID" ]; then
@@ -57,17 +30,31 @@ configure_scope() {
 configure_scope
 
 # Enable the Google Cloud APIs needed for misconfiguration scanning
-run_command "gcloud services enable \
+gcloud services enable \
     iam.googleapis.com \
     deploymentmanager.googleapis.com \
     cloudresourcemanager.googleapis.com \
-    cloudasset.googleapis.com"
+    cloudasset.googleapis.com
 
-# Apply the deployment manager templates
-result="$(run_command "gcloud deployment-manager deployments create --automatic-rollback-on-error ${DEPLOYMENT_NAME} --project ${PROJECT_NAME} \
+result="$(gcloud deployment-manager deployments create --automatic-rollback-on-error ${DEPLOYMENT_NAME} --project ${PROJECT_NAME} \
     --template service_account.py \
-    --properties scope:${SCOPE},parentId:${PARENT_ID}")"
+    --properties scope:${SCOPE},parentId:${PARENT_ID})"
 
-echo -e "$(echo "$result" | grep -o 'serviceAccountKey .*' | awk '{print $2}' | base64 -d >KEY_FILE.json)"
-echo -e "\nRun 'cat KEY_FILE.json' to view the service account key. Copy and paste it in the CIS GCP integration."
-echo -e "\n\nIt is recommended to also save the key in a secure location for future use."
+key="$(echo "$result" | grep -o 'serviceAccountKey .*' | awk '{print $2}')"
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
+if [ -z "$key" ]; then
+    echo "${RED}Error: Failed to deploy a service account. Exiting...${RESET}"
+    exit 1
+fi
+
+echo -e "\n${GREEN}Deployment complete.${RESET}\n"
+
+gcloud deployment-manager deployments describe ${DEPLOYMENT_NAME} --format='table(resources)'
+
+echo -e "$(echo "$key" | base64 -d >KEY_FILE.json)"
+echo -e "\n${GREEN}Run 'cat KEY_FILE.json' to view the service account key. Copy and paste it in the CIS GCP integration."
+echo -e "\nYou should also save the key in a secure location for future use.${RESET}"
